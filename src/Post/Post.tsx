dabce34c9e6b9dbae2b0e880f790cac315e61fb8
RomanKhamidov <khamidovroman92@gmail.com>
2026-06-03 15:55:22 +0600

    developer/login branch commit - 2

--- src/Post/Post.tsx
+++ src/Post/Post.tsx
@@ -5,6 +5,7 @@ export const User = () => {
         <div>
             <h2>User22d2</h2>
             <h2>developer/login branch commit - 1</h2>
+            <h2>developer/login branch commit - 2</h2>
         </div>
     )
 }
